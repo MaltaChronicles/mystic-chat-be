da86cde4a2bacee88a2b45817a77751c879ca361
Malta <maltachronicles@gmail.com>
2023-11-27 14:05:28 +0100

    Aggiunti gruppi Swagger

---
+++ src/main/java/it/mystic/chat/config/SwaggerConfiguration.java
@@ -0,0 +1,33 @@
+package it.mystic.chat.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public GroupedOpenApi config(){
+        return GroupedOpenApi.builder()
+                .group("Config")
+                .pathsToMatch("/**/config/**/")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi off(){
+        return GroupedOpenApi.builder()
+                .group("OFF")
+                .pathsToMatch("/**/off/**/")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi on(){
+        return GroupedOpenApi.builder()
+                .group("ON")
+                .pathsToMatch("/**/on/**/")
+                .build();
+    }
+}
