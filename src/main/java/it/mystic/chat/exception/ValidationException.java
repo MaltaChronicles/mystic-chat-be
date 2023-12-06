168a01ad5e8352b6f3280552d8eda34f80060ce9
Malta <maltachronicles@gmail.com>
2023-12-06 18:54:48 +0100

    Some change Validation

--- src/main/java/it/mystic/chat/exception/ValidationException.java
+++ src/main/java/it/mystic/chat/exception/ValidationException.java
@@ -2,11 +2,13 @@ package it.mystic.chat.exception;
 
 import it.mystic.chat.model.response.InvalidParam;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.Collections;
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class ValidationException extends jakarta.validation.ValidationException {
 
     private List<InvalidParam> violations;
